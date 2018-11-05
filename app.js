d72e66c01c887ebc7e47a96e75df9cf50ffa79e5
MansourMoukdad88 <m_sy@outlook.com>
2018-11-05 20:32:41 +0200

    fixed

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-// TODO
+
+
+var App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList groceryItems={['Cucumbers', 'Kale', 'Apple']}/>
+  </div>
+);
+
+var GroceryList = (props) => (
+  <ul>
+    {props.groceryItems.map(groceryItem =>
+      <GroceryListItem groceryItem={groceryItem} />
+    )}
+  </ul>
+);
+
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.groceryItem}</li>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
